1b05c798b1521287dce6052f5b4c45aeecbdd675
dylandupasquier <dylandupasquier@gmail.com>
2022-12-20 10:26:13 -0800

    remove dead code

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import 'App.scss';
 
 import { GameContext } from 'contexts/GameContext';
 import Input from 'components/MainForm';
-import Connecting from 'components/Connecting';
 
 function App() {
   const { gameState, setGameState, glitching, game } =
